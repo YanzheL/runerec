955201d5888e69eb572fc0bab66758fdab34f56b
YanzheL <lee.yanzhe@yanzhe.org>
2018-10-19 02:42:48 -0700

    refine settings

---
+++ Classifiers/Classifier_Internal.h
@@ -0,0 +1,8 @@
+//
+// Created by Trinity on 19/10/2018.
+//
+
+#ifndef RUNEREC_CLASSIFIER_INTERNAL_H
+#define RUNEREC_CLASSIFIER_INTERNAL_H
+
+#endif //RUNEREC_CLASSIFIER_INTERNAL_H
---
+++ Splitters/Splitter.h
@@ -0,0 +1,8 @@
+//
+// Created by Trinity on 19/10/2018.
+//
+
+#ifndef RUNEREC_SPLITTERS_H
+#define RUNEREC_SPLITTERS_H
+
+#endif //RUNEREC_SPLITTERS_H
